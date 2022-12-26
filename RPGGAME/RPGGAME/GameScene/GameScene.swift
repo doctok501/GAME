96c88550999dbb7ed7edb789accbf50a3af8b069
kim_ingyeom <doctok501@gmail.com>
2022-12-26 13:52:43 +0900

    RPG

---
+++ RPGGAME/RPGGAME/GameScene/GameScene.swift
@@ -0,0 +1,23 @@
+//
+//  GameScene.swift
+//  RPGGAME
+//
+//  Created by 김인겸 on 2022/11/27.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    let Player = SKSpriteNode(imageNamed: "RedSwan")
+    
+    
+
+    
+    override func didMove(to view: SKView) {
+        self.addChild(Player)
+        
+    }
+    
+}
